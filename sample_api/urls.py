03c9ec93ce12a3624bc1811313fb47fd50abf337
MrJuicyBacon <iambacon@yandex.ru>
2019-11-23 02:47:36 +0300

    CategoryCreateView added at "categories/"

--- sample_api/urls.py
+++ sample_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path(r'categories/', views.CategoryCreateView.as_view()),
     path(r'categories/<int:pk>/', views.CategoryRetrieveView.as_view()),
 ]
--- sample_api/views.py
+++ sample_api/views.py
@@ -1,9 +1,14 @@
 from .models import Category
 from rest_framework import generics
-from .serializers import CategoryRetrieveSerializer
+from .serializers import CategoryRetrieveSerializer, CategoryCreateSerializer
 
 
 #  View for retrieving a single category
 class CategoryRetrieveView(generics.RetrieveAPIView):
     serializer_class = CategoryRetrieveSerializer
     queryset = Category.objects.all()
+
+
+# View for creating categories (including nested ones)
+class CategoryCreateView(generics.CreateAPIView):
+    serializer_class = CategoryCreateSerializer
